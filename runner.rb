af98c478c03882c423b34558072ab3ddbd9e158e
sts10 <sschlinkert@gmail.com>
2016-05-05 00:00:31 -0400

    initial commit

---
+++ runner.rb
@@ -0,0 +1,85 @@
+require 'pry'
+require 'nokogiri'
+require 'rss'
+require 'open-uri'
+require 'csv'
+
+html_url = "http://www.theawl.com"
+xml_url = "http://www.theawl.com/feed"
+rss_url = "http://feeds2.feedburner.com/TheAwl"
+
+class Page
+  attr_reader :posts
+  def initialize(url)
+    @doc = Nokogiri::HTML(open(url))
+    @posts = []
+    self.make_posts
+  end
+
+  def make_posts
+    @doc.search("div.reverse-chron__post").each do |post|
+      if !post.css("div.post__body div p:first img:first").empty?
+        this_post = Post.new(post)
+        @posts << this_post
+      end
+    end
+  end
+end
+
+class Post
+  attr_reader :image_src, :image_alt, :post_url
+  def initialize(post)
+    post_image = post.css("div.post__body div p:first img:first")
+    @image_src = post_image.attr("src")&.value
+    @image_alt = post_image.attr("alt")&.value
+    @post_url = post.css('h2 a').attr('href')&.value
+  end
+end
+
+base_url = "http://www.theawl.com/page/"
+
+all_posts = []
+
+# 2707 is last page as of today
+total_number_of_pages_to_scrape = 2705
+
+15.times do |i|
+  i = i + 1
+  this_page_url = base_url + (i+200).to_s
+  
+  this_page = Page.new(this_page_url)
+  all_posts = all_posts + this_page.posts
+
+  puts "sleeping 5 before going to next page"
+  sleep 5
+end
+
+with_src = 0
+with_alt = 0
+with_post_url = 0
+
+all_posts.each do |post|
+  with_src = with_src +1 if post.image_src
+  with_alt = with_alt +1 if post.image_alt
+  with_post_url = with_post_url +1 if post.post_url
+end
+
+puts "total number of post: #{all_posts.size}"
+puts "total number of posts with src: #{with_src}"
+puts "total number of posts with alt: #{with_alt}"
+puts "total number of posts with post url: #{with_post_url}"
+
+CSV.open("archive1.csv", "w") do |csv|
+  all_posts.each do |post|
+    csv << [post.post_url, post.image_src, post.image_alt]
+  end
+end
+
+
+CSV.open("just_with_alt.csv", "w") do |csv|
+  all_posts.each do |post|
+    if post.image_alt && post.image_alt != "" && post.image_alt != '""'
+      csv << [post.post_url, post.image_src, post.image_alt]
+    end
+  end
+end
